562df734166ec75f6b9329b1b1f253aad3f2aa52
isrooky <israel.campo00@csioviedo.com>
2025-07-05 19:14:20 +0200

    COntroles roca y amcara

--- Assets/1-Scripts/CinemachineSetter.cs
+++ Assets/1-Scripts/CinemachineSetter.cs
@@ -5,29 +5,41 @@ using UnityEngine;
 public class CinemachineSetter : MonoBehaviour
 {
     [SerializeField] CinemachineCamera cinemachineCamera;
-    [SerializeField] CinemachineOrbitalFollow cinemachineOrbitalFollow;
     [SerializeField] TrackerSettings bindingMode;
     [SerializeField] Vector3 targetOffset;
     [SerializeField] float radius;
     [SerializeField] InputAxis horizontalAxis;
     [SerializeField] InputAxis verticalAxis;
     [SerializeField] InputAxis radialAxis;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-    [ContextMenu("Setter")]
-    void SetValues()
-    {
-        cinemachineOrbitalFollow.TrackerSettings = bindingMode;
-        cinemachineOrbitalFollow.TargetOffset = targetOffset;
-        cinemachineOrbitalFollow.Radius = radius;
 
-    }
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        ForceSwitchOrbital();
+    }
+    [ContextMenu("Force Switch Orbital")]
+    public void ForceSwitchOrbital()
+    {
+        RemoveProcedural<CinemachineOrbitalFollow>();
+        RemoveProcedural<CinemachineThirdPersonFollow>();
+
+        // Añade ThirdPerson y luego Orbital para forzar el bugfix
+        var tps = cinemachineCamera.gameObject.AddComponent<CinemachineThirdPersonFollow>();
+
+        Destroy(tps); // Lo quitamos de inmediato
+
+        var orbital = cinemachineCamera.gameObject.AddComponent<CinemachineOrbitalFollow>();
+        orbital.TrackerSettings = bindingMode;
+        orbital.TargetOffset = targetOffset;
+        orbital.Radius = radius;
+        orbital.HorizontalAxis = horizontalAxis;
+        orbital.VerticalAxis = verticalAxis;
+        orbital.RadialAxis = radialAxis;
+    }
+
+    void RemoveProcedural<T>() where T : Component
+    {
+        var comp = cinemachineCamera.GetComponent<T>();
+        if (comp != null)
+            DestroyImmediate(comp);
     }
 }
--- Assets/1-Scripts/SimpleWASDController.cs
+++ Assets/1-Scripts/SimpleWASDController.cs
@@ -1,8 +1,10 @@
 using UnityEngine;
 
-public class SimpleWASDController : MonoBehaviour
+public class SimpleWASDCameraController : MonoBehaviour
 {
     public float moveSpeed = 8f;
+    public Transform cameraTransform; // Arrastra aquí tu cámara (MainCamera o la Cinemachine)
+
     private Rigidbody rb;
 
     void Awake()
@@ -12,14 +14,23 @@ public class SimpleWASDController : MonoBehaviour
 
     void FixedUpdate()
     {
-        // Leer input
         float h = Input.GetAxis("Horizontal"); // A/D o Flechas
         float v = Input.GetAxis("Vertical");   // W/S o Flechas
 
-        // Vector de movimiento en plano XZ
-        Vector3 move = new Vector3(h, 0, v) * moveSpeed;
+        // Obtener forward y right de la cámara en plano XZ
+        Vector3 camForward = cameraTransform.forward;
+        Vector3 camRight = cameraTransform.right;
 
-        // Mantener la velocidad vertical (ca�da/gravedad)
+        // Ignorar el eje Y (evitar que suba o baje)
+        camForward.y = 0f;
+        camRight.y = 0f;
+        camForward.Normalize();
+        camRight.Normalize();
+
+        // Calcular movimiento relativo a la cámara
+        Vector3 move = (camForward * v + camRight * h) * moveSpeed;
+
+        // Mantener la velocidad vertical (gravedad)
         Vector3 velocity = rb.linearVelocity;
         velocity.x = move.x;
         velocity.z = move.z;
